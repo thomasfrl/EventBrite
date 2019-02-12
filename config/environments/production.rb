0188644d29df0be9258472bcfaaec665d55af975
thomasfrl <feral.thomas@gmail.com>
2019-02-12 19:01:36 +0100

    pas trop mal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #config devise mailer in production 
+  config.action_mailer.default_url_options = { :host => 'YOURAPPNAME.herokuapp.com' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
-  root to: 'home#index'
+  get 'users/show'
+  root to: 'events#index'
   get 'home/index'
   get 'home/secret'
   devise_for :users
+  resources :events
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
