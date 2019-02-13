f24aadd685d11cdac30a4df2deba6c0e8e0e5a5d
thomasfrl <feral.thomas@gmail.com>
2019-02-13 12:27:56 +0100

    test heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
-  #after_create :welcome_send
+  after_create :welcome_send
 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
