0af38cce81ad4e76bcd2d5877741b34074c4eb79
thomasfrl <feral.thomas@gmail.com>
2019-02-13 12:22:49 +0100

    stripe et heroku

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :events
   resources :users
+  resources :charges
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
