e312debcef31da6527349d755c28bd0f31b82785
thomasfrl <feral.thomas@gmail.com>
2019-02-20 01:59:14 +0100

    petites modifs

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,15 @@ Attendance.destroy_all
 Event.destroy_all
 User.destroy_all
 
+text = "
+Quanta autem vis amicitiae sit, ex hoc intellegi maxime potest, quod ex infinita societate generis humani, quam conciliavit ipsa natura, ita contracta res est et adducta in angustum ut omnis caritas aut inter duos aut inter paucos iungeretur.
+<br /><br />
+
+Proinde die funestis interrogationibus praestituto imaginarius iudex equitum resedit magister adhibitis aliis iam quae essent agenda praedoctis, et adsistebant hinc inde notarii, quid quaesitum esset, quidve responsum, cursim ad Caesarem perferentes, cuius imperio truci, stimulis reginae exsertantis aurem subinde per aulaeum, nec diluere obiecta permissi nec defensi periere conplures.
+<br /><br />
+
+Homines enim eruditos et sobrios ut infaustos et inutiles vitant, eo quoque accedente quod et nomenclatores adsueti haec et talia venditare, mercede accepta lucris quosdam et prandiis inserunt subditicios ignobiles et obscuros.
+"
 def get_random_image(image_path_prefix)
   image_files = Dir.glob("#{image_path_prefix}*")
   image_files.sample.split(image_path_prefix)[1]
@@ -26,7 +35,7 @@ puts "Users created"
 
 
 15.times do
-  event = Event.create!(duration: rand(1..200)*5, start_date: Time.now + rand(3600..360000), title: Faker::Book.title, description: "Encore un evenement qui casse des culs par rangées de 12", price: rand(1..1000), location: Faker::Nation.capital_city, administrator_id: User.all.sample.id, validated: true)
+  event = Event.create!(duration: rand(1..200)*5, start_date: Time.now + rand(3600..360000), title: Faker::Book.title, description: text, price: rand(1..1000), location: Faker::Nation.capital_city, administrator_id: User.all.sample.id, validated: true)
   event.event_image.attach(io: File.open("app/assets/images/events/sample/#{get_random_image('app/assets/images/events/sample/')}"), filename: get_random_image('app/assets/images/events/sample/'))
 
 end
